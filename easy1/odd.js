c743c26e530c7f50370abd6c537ca79ad0a6bdea
Jason Grand <grandja@umich.edu>
2021-05-26 13:45:54 -0400

    easy1 exercises

--- easy1/odd.js
+++ easy1/odd.js
@@ -1,30 +1,8 @@
-function isOdd(number) {
-  // Edge case 
-  if (number === 0) { 
-    return false; 
-  }
-  
-  // Test if even or odd
-  if (Math.abs(number) % 2 === 0) {
-    return false;
-  }
-  else {
-    return true;
+funtion logOdd() {
+  for (let i = 1; i < 100; i+=2) {
+    console.log(i);
   }
 }
 
-
-// Test cases
-console.log(isOdd(2)); // => false
-console.log(isOdd(5)); // => true
-console.log(isOdd(-17)); // => true
-console.log(isOdd(-8)); // => false
-console.log(isOdd(0)); // => false
-console.log(isOdd(7)); // => true
-
-
-/* Solution
-function isOdd(number) {
-  return Math.abs(number) % 2 === 1;
-}
-*/
+// Test
+logOdd();
